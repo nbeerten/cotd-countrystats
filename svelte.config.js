dc91f3ca946352949cdafb670aaa08542a3cfc3f
nbeerten <59444993+nbeerten@users.noreply.github.com>
2023-05-08 13:44:33 +0200

    Remove split functions

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapterVercel({
             runtime: 'edge',
-            regions: ['fra1'],
-            split: true,
+            regions: ['fra1']
         }),
     },
 };
