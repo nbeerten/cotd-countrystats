b77cf1ba1cbbdbdab4ef58769f7983f99b7d8046
nbeerten <59444993+nbeerten@users.noreply.github.com>
2023-05-08 13:49:55 +0200

    Remove getZones KV

--- src/routes/api/getZones/+server.ts
+++ src/routes/api/getZones/+server.ts
@@ -1,41 +1,10 @@
 import type { RequestHandler } from './$types';
-import kv from '@vercel/kv';
-import { NadeoServices, type ZonesResponse } from '$lib/server/nadeo';
+import { NadeoServices } from '$lib/server/nadeo';
 
 export const GET: RequestHandler = async () => {
     const NadeoServicesClient = await NadeoServices;
 
-    console.time('/api/getZones');
-    const NADEO_ZONES = 'NADEO_ZONES';
-
-    let zones: ZonesResponse;
-    if (await kv.exists(NADEO_ZONES)) {
-        const zonesRes = await kv.get<ZonesResponse>(NADEO_ZONES);
-
-        if (zonesRes && 'zoneId' in zonesRes[0]) {
-            console.debug('Using cached zones from /api/getZones.');
-            zones = zonesRes;
-        } else {
-            console.warn(
-                `Failed to use cached zones. Missing 'zoneId' from vercel KV stored ${NADEO_ZONES} key.`
-            );
-
-            await kv.del(NADEO_ZONES);
-            zones = await NadeoServicesClient.getZones();
-
-            await kv.set(NADEO_ZONES, zones, {
-                ex: 60 * 60 * 24 * 7,
-            });
-        }
-    } else {
-        zones = await NadeoServicesClient.getZones();
-
-        await kv.set(NADEO_ZONES, zones, {
-            ex: 60 * 60 * 24 * 7,
-        });
-    }
-
-    console.timeEnd('/api/getZones');
+    const zones = await NadeoServicesClient.getZones();
 
     return new Response(JSON.stringify(zones), {
         headers: {
